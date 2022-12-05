7bb012c8b055f8a6cdd124dcf368e37cc37aa073
ayumiohno <ayumi0130ohno@gmail.com>
2022-12-05 20:27:12 +0900

    key-bindings for bufferline

--- lua/plugin/lspconfig.lua
+++ lua/plugin/lspconfig.lua
@@ -1,4 +1,8 @@
 return function()
+	local opts = { noremap = true, silent = true }
+	vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+	vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+	vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 	function on_attach(client, bufnr)
 		local bufopts = { buffer = bufnr }
 		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
